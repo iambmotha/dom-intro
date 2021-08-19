1785e7f22abe338169d502de72fbf186c5f702b9
Bongani <iambmotha@gmail.com>
2021-08-20 00:20:38 +0200

    added progress in radio and text bill

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,40 @@
-// get a reference to the textbox where the bill type is to be entered
 
 //get a reference to the add button
-
+const textTotalAddBtn = document.querySelector(".addToBillBtn")
 //create a variable that will keep track of the total bill
+var billTypeText = document.querySelector('.billTypeText');
+var callTotalOneElem = document.querySelector('.callTotalOne');
+var smsTotalOneElem = document.querySelector('.smsTotalOne');
+var totalOneElem = document.querySelector('.totalOne');
 
-//add an event listener for when the add button is pressed
+var callsTotal = 0;
+var smsTotal = 0;
 
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+textTotalAddBtn.addEventListener('click', function(){
+
+    
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value;
+  
+    // update the correct total
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callTotalOneElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalOneElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalOneElem.innerHTML = totalCost.toFixed(2);
+
+
+    if (totalCost >= 50){
+        totalOneElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalOneElem.classList.add("warning");
+    }
+});
